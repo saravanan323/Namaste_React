40f23bbaaf636695f01607e2fa772300a4bc4092
its_saravanan <saravanan.s@inflexiontechfze.com>
2025-03-28 09:07:07 +0530

    changes

--- Episode_05/Code/src/components/Body.js
+++ Episode_05/Code/src/components/Body.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import RestaurentContainer from "./RestaurantContainer";
 
-const Body = () => (
-  <div className="body">
-    <div className="search-container">
-      <input className="search" placeholder="Search......." />
+const Body = () => {
+  const [search, setSearch] = useState();
+
+  return (
+    <div className="body">
+      <div className="search-container">
+        <input className="search" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search......." />
+      </div>
+      <RestaurentContainer search={search} />
     </div>
-    <RestaurentContainer />
-  </div>
-);
+  )
+};
 
 export default Body;
--- Episode_05/Code/src/components/RestaurantContainer.js
+++ Episode_05/Code/src/components/RestaurantContainer.js
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../utils/MockData";
 import RestaurantCard from "./RestaurantCard";
 
-const RestaurentContainer = () => {
+const RestaurentContainer = (props) => {
   const [list, setList] = useState(resList);
+  const search = props;
+
+  useEffect(() => {
+    if (search.search) {
+      const oFilteredList = resList.filter(e => e.info?.name.includes(search.search));
+      setList(oFilteredList);
+    } else {
+      setList(resList); // Reset to original list when search is empty
+    }
+  }, [search.search, resList]); // Runs when search or resList changes
 
   return (
-    
+
     <div className="res-container">
       {list.map((e) => (
         <RestaurantCard key={e.info.id} resData={e} />
