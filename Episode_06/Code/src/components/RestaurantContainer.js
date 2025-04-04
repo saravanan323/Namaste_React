eae9fb310e3e48f24000e7364af9265385014484
its_saravanan <saravanan.s@inflexiontechfze.com>
2025-04-04 08:26:27 +0530

    changes

--- Episode_06/Code/src/components/RestaurantContainer.js
+++ Episode_06/Code/src/components/RestaurantContainer.js
@@ -4,16 +4,16 @@ import Shimmer from "./Shimmer";
 
 const RestaurentContainer = (props) => {
   const [list, setList] = useState([]);
-  const search = props;
+  const [filteredList, setFilteredList] = useState([]);
 
-  // useEffect(() => {
-  //   if (search.search) {
-  //     const oFilteredList = resList.filter(e => e.info?.name.includes(search.search));
-  //     setList(oFilteredList);
-  //   } else {
-  //     setList(resList); // Reset to original list when search is empty
-  //   }
-  // }, [search.search, resList]); // Runs when search or resList changes
+  useEffect(() => {
+    if (props.search) {
+      const oFilteredList = list.filter(e => e.info?.name.toLowerCase().includes(props.search.toLowerCase()));
+      setFilteredList(oFilteredList);
+    } else {
+      setFilteredList(list); // Reset to original list when search is empty
+    }
+  }, [props.search, filteredList]); // Runs when search or resList changes
 
   useEffect(() => {
     fetchData();
@@ -23,15 +23,16 @@ const RestaurentContainer = (props) => {
     const sUrl = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9966024&lng=77.6687231"
     const data = await fetch(sUrl);
     const json = await data.json();
-    const filteredList = json.data.cards.map(e => e.card.card?.gridElements?.infoWithStyle?.restaurants).filter(e => e)[0];
+    const oFilteredList = json.data.cards.map(e => e.card.card?.gridElements?.infoWithStyle?.restaurants).filter(e => e)[0];
     console.log(json.data.cards.map(e => e.card.card?.gridElements?.infoWithStyle?.restaurants).filter(e => e)[0]);
-    setList(filteredList);
+    setList(oFilteredList);
+    setFilteredList(oFilteredList);
   }
 
   // Handle case when list is not yet populated
   return list.length === 0 ? (<Shimmer />) : ( //Condtional rendering
     <div className="res-container">
-      {list.map((e) => (
+      {filteredList.map((e) => (
         <RestaurantCard key={e.info.id} resData={e} />
       ))}
     </div>
